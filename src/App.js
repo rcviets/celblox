a35a58dc62d34fab90ecfa6a31debc95e7bfb199
Randamus <rcviets@gmail.com>
2020-08-05 21:31:27 -0600

    nav placeholder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Hero from './components/splashHero'
+import Nav from './components/nav';
+import Hero from './components/splashHero';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Nav />
       <Hero />
     </div>
   );
