7e5c0141d97c2a0fd13ee0cb037aa3d5e1b4b604
Randamus <rcviets@gmail.com>
2020-08-05 22:45:54 -0600

    styling

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,20 +1,21 @@
 import React from "react";
+import "./style.css";
 import Navbar from "react-bootstrap/Navbar";
 
 const Nav = () => {
     return (
-        <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src="/logo.png"
-        width="319px"
-        height="60px"
-        className="d-inline-block align-top"
-      />{' '}
-      React Bootstrap
-    </Navbar.Brand>
-  </Navbar>
+            <Navbar fixed="top" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt="navbar"
+                        src="/logo.png"
+                        width="319px"
+                        height="60px"
+                        className="d-inline-block align-bottom"
+                    />{' '}
+                    The Future Is Coming Together
+                </Navbar.Brand>
+            </Navbar>
     );
 };
 
--- src/components/splashHero/index.js
+++ src/components/splashHero/index.js
@@ -1,18 +1,20 @@
 import React from "react";
+import "./style.css"
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 
 const Hero = () => {
   return (
-    <Jumbotron fluid>
-      <Container>
-        <h1>Fluid jumbotron</h1>
-        <p>
-          This is a modified jumbotron that occupies the entire horizontal space of
-          its parent.
-    </p>
-      </Container>
-    </Jumbotron>
+    <div className="splashHero">
+      <Jumbotron fluid>
+        <Container>
+          <h1>Fluid jumbotron</h1>
+          <p>
+            This is a modified jumbotron that occupies the entire horizontal space of
+            its parent.</p>
+        </Container>
+      </Jumbotron>
+    </div>
   );
 };
 
