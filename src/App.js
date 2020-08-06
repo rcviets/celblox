4c5129bc458dc36552310e8467deb379987cfacf
Randamus <rcviets@gmail.com>
2020-08-05 21:22:31 -0600

    generic hero jumbotron

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './components/splashHero'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
