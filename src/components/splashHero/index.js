548014759918c2f4f95c959ae3b3bc41be69e6a3
Randamus <rcviets@gmail.com>
2020-08-06 21:37:12 -0600

    learn more button

--- src/components/splashHero/index.js
+++ src/components/splashHero/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import "./style.css"
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button'
 
 const Hero = () => {
   return (
     <div className="splashHero">
       <Jumbotron fluid>
         <Container>
-          <h1>Fluid jumbotron</h1>
-          <p>
-            This is a modified jumbotron that occupies the entire horizontal space of
-            its parent.</p>
+          <Button className="button" size="lg">Learn More</Button>{' '}
+          <p></p>
         </Container>
       </Jumbotron>
     </div>
