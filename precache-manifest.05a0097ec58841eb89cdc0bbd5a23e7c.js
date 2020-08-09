957c2b6fffe393ff491b133fcd2e8cc3fdf23e81
Randamus <rcviets@gmail.com>
2020-08-09 15:16:43 -0600

    Updates

---
+++ precache-manifest.05a0097ec58841eb89cdc0bbd5a23e7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8496ccd6d71dad53a662da7fd6fec9",
+    "url": "/%7Bcelblox%7D/index.html"
+  },
+  {
+    "revision": "0b8caa861b884f2d73c0",
+    "url": "/%7Bcelblox%7D/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "6908007f16f92cb9c2f2",
+    "url": "/%7Bcelblox%7D/static/css/main.2e1a1947.chunk.css"
+  },
+  {
+    "revision": "0b8caa861b884f2d73c0",
+    "url": "/%7Bcelblox%7D/static/js/2.fd6a4a51.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/%7Bcelblox%7D/static/js/2.fd6a4a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6908007f16f92cb9c2f2",
+    "url": "/%7Bcelblox%7D/static/js/main.acaf3b8c.chunk.js"
+  },
+  {
+    "revision": "319ccd065ac4990a669c",
+    "url": "/%7Bcelblox%7D/static/js/runtime-main.4b10f42d.js"
+  },
+  {
+    "revision": "5e44a554c48af24be0df6408ad2bcd61",
+    "url": "/%7Bcelblox%7D/static/media/splashHero.5e44a554.png"
+  },
+  {
+    "revision": "89a0298fd46b861229d8f99a19a74833",
+    "url": "/%7Bcelblox%7D/static/media/wave.89a0298f.svg"
+  }
+]);
