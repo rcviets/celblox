dcfd9672b910901bd69c2c64fb794c5020dee674
Randamus <rcviets@gmail.com>
2020-08-08 21:43:26 -0600

    navlinks styling

--- src/components/navLinks/index.js
+++ src/components/navLinks/index.js
@@ -4,20 +4,15 @@ import "./style.css";
 
 const NavLinks = () => {
     return (
-        <Nav className="justify-content-end" activeKey="/home">
-            <Nav.Item>
-                <Nav.Link href="/home">Active</Nav.Link>
+        <Nav className="justify-content-end">
+            <Nav.Item className="navLink">
+                <Nav.Link eventKey="link-1" style={{color: "#451F55"}}>Customers</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
+            <Nav.Item className="navLink">
+                <Nav.Link eventKey="link-2" style={{color: "#451F55"}}>Partners</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                    Disabled
-                </Nav.Link>
+            <Nav.Item className="navLink">
+                <Nav.Link eventKey="link-2" style={{color: "#451F55"}}>Resources</Nav.Link>
             </Nav.Item>
         </Nav>
     );
