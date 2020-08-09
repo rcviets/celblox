df68650da42a5aff4f8e828f849d67492651368d
Randamus <rcviets@gmail.com>
2020-08-09 14:49:49 -0600

    favicon

--- src/components/splashHero/index.js
+++ src/components/splashHero/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import "./style.css"
 
 const Hero = () => {
@@ -10,6 +11,11 @@ const Hero = () => {
         <Container>
           <h1 className="header1">Sustainable. Durable.</h1>
           <h1 className="header2">Cost Effective.</h1>
+          <div className="button">
+            <Button className="buttonLM" size="lg">
+              Learn More
+            </Button>{' '}
+          </div>
         </Container>
       </Jumbotron>
     </div>
