548d913e53b8994933e837905316cf5873229328
Randamus <rcviets@gmail.com>
2020-08-19 21:45:29 -0600

    products header and button

--- src/components/splashProduct/index.js
+++ src/components/splashProduct/index.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import Button from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import "./style.css";
 
 const ProductHero = () => {
     return (
-        <Jumbotron fluid>
-            <Container>
-                <h1>Block Dimensions</h1>
-                <p>
-                    This is a modified jumbotron that occupies the entire horizontal space of
-                    its parent.
-    </p>
-            </Container>
-        </Jumbotron>
+        <Container fluid>
+            <Jumbotron className="products-jumbotron" fluid>
+                <Row>
+                    <Col md={9}>
+                        <h1 className="products-headline">Block Dimensions</h1>
+                    </Col>
+                    <Col md={3}>
+                        <Button className="rounded-0" size="lg" block>Request A Quote</Button>
+                    </Col>
+                </Row>
+                <hr className="line-break-one"></hr>
+            </Jumbotron>
+        </Container>
     );
 };
 
